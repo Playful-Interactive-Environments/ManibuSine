fb7cfab05c1d13d63f67bb8556655f2bdbdce526
GeorgiKostov <georgikostov1337@gmail.com>
2016-03-21 15:55:20 +0100

    Added Utilities class New Scenes for testing

---
+++ Assets/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject BallPrefab;
+	private int _ballsAmount;
+
+	void Start () {
+		InvokeRepeating("SpawnBall", 1.0f, 0.1f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_ballsAmount > 200)
+		{
+			CancelInvoke("SpawnBall");
+		}
+	}
+	void SpawnBall()
+	{
+		
+		Instantiate(BallPrefab, transform.position, Quaternion.identity);
+		_ballsAmount++;
+		
+	}
+}
