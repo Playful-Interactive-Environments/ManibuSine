39f785eb3dc35ae13b29d5571de626995ff37f07
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-04-16 17:30:50 +0200

    canon trigger test

---
+++ Assets/CanonTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CanonTrigger : NetworkBehaviour {
+
+    [SyncVar]
+    public GameObject assignedPlayer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player" && assignedPlayer == null) {
+            assignedPlayer = other.gameObject;
+        }
+    }
+
+    void OnTriggerExti(Collider other)
+    {
+        assignedPlayer = null;
+    }
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
