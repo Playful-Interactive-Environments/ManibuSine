525785f330495d5af37157e99b42dc33183d9e41
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-04-25 17:52:48 +0200

    cmd test2

--- Assets/NetworkDataManager.cs
+++ Assets/NetworkDataManager.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class NetworkDataManager : NetworkBehaviour {
+public class NetworkDataManager : NetworkBehaviour
+{
     //private static NetworkDataManager instance;
     //public static NetworkDataManager Instance { get { return instance;}}
 
@@ -21,22 +22,27 @@ public class NetworkDataManager : NetworkBehaviour {
     public void CmdShoot()
     {
         print("NDM: CmdShoot");
-        if(EventShoot != null)
+        if (EventShoot != null)
         {
             EventShoot();
         }
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+            if (Input.GetKeyDown(KeyCode.Space))
+                CmdShoot();
         //if (ServerManager.Instance.isServer)
         //{
         //    NetworkServer.Spawn(gameObject);
         //}
-	}
+    }
 }
