7226bbe9bf419fbf883b74df1573c9b03d37004c
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-04-25 16:26:28 +0200

    spawn networkDataManager

--- Assets/NetworkDataManager.cs
+++ Assets/NetworkDataManager.cs
@@ -36,6 +36,9 @@ public class NetworkDataManager : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (ServerManager.Instance.isServer)
+        {
+            NetworkServer.Spawn(gameObject);
+        }
 	}
 }
