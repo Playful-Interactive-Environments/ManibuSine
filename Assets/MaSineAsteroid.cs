f1d4056d3a8283a1a85fa52df746e899fea5ad5b
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-04-28 16:48:30 +0200

    destroy asteroid

---
+++ Assets/MaSineAsteroid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MaSineAsteroid : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isServer)
+            Invoke("KillMe", 3);
+	}
+
+    void KillMe()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
