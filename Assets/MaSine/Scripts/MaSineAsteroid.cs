2f08073ae5b5c7c4ec9da64590c6b59662a6d0bc
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-05-04 17:43:28 +0200

    set rand vel only if server

--- Assets/MaSine/Scripts/MaSineAsteroid.cs
+++ Assets/MaSine/Scripts/MaSineAsteroid.cs
@@ -5,10 +5,11 @@ using UnityEngine.Networking;
 public class MaSineAsteroid : NetworkBehaviour {
 
     NetworkPlayer player;
-    private float speed = 1000.0f;
+    private float speed = 100.0f;
 
 	// Use this for initialization
 	void Start () {
+        if(isServer)
         GetComponent<Rigidbody>().velocity = Random.onUnitSphere * speed;
 	}
 
