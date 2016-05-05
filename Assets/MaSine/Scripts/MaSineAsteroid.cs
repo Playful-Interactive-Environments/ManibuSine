f0f775694fc92b63d8167fe127ada7da114aedd2
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-05-05 13:50:53 +0200

    changed network player (shader, meshes)

--- Assets/MaSine/Scripts/MaSineAsteroid.cs
+++ Assets/MaSine/Scripts/MaSineAsteroid.cs
@@ -5,12 +5,12 @@ using UnityEngine.Networking;
 public class MaSineAsteroid : NetworkBehaviour {
 
     NetworkPlayer player;
-    private float speed = 100.0f;
+    private float speed = 10000.0f;
 
 	// Use this for initialization
 	void Start () {
-        if(isServer)
-        GetComponent<Rigidbody>().velocity = Random.onUnitSphere * speed;
+        if (isServer)
+            GetComponent<Rigidbody>().AddForce(Random.onUnitSphere * speed);
 	}
 
     void OnTriggerEnter(Collider other)
