b2de7a35d9e6152052389e08fae06b931892fe31
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-05-05 16:08:16 +0200

    changed asteroid, added audioManager added explosion sound to destroyed asteroid

---
+++ Assets/MaSine/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour {
+
+    private static AudioManager instance;
+    public static AudioManager Instance { get { return instance; } }
+
+    public List<AudioClip> clips;
+
+    void Awake()
+    {
+        instance = this;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
