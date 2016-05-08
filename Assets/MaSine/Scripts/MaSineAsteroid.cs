a056c00c689c8264bfb3d3723faa61154fdb1f26
unknown <dnl.rammer@gmail.com>
2016-05-08 21:34:08 +0200

    changed materials, play explosion particles, changed player geometry, added point light to player, added random rotation to asteroids

--- Assets/MaSine/Scripts/MaSineAsteroid.cs
+++ Assets/MaSine/Scripts/MaSineAsteroid.cs
@@ -7,13 +7,18 @@ public class MaSineAsteroid : NetworkBehaviour {
     NetworkPlayer player;
     private float speed = 10000.0f;
     private AudioManager audioManager;
+    private float rotSpeed;
+
+    public GameObject explosionParticles;
 
 	// Use this for initialization
 	void Start () {
         
         audioManager = AudioManager.Instance;
-        if (isServer)
+        if (isServer) {
             GetComponent<Rigidbody>().AddForce(Random.onUnitSphere * speed);
+            rotSpeed = Random.Range(-0.05f, 0.05f) * 3.0f;
+        }
 	}
 
     void OnTriggerEnter(Collider other)
@@ -23,17 +28,20 @@ public class MaSineAsteroid : NetworkBehaviour {
         if (isServer)
         {
             Destroy(gameObject);
-
         }
     }
 
     public override void OnNetworkDestroy()
     {
         audioManager.PlayClipAt(audioManager.clips[0], transform.position);
+
+        if (!isServer)
+            Instantiate(explosionParticles, transform.position, Quaternion.identity);
     }
 
 	// Update is called once per frame
 	void Update () {
-	
+        if (isServer)
+            transform.Rotate(rotSpeed, rotSpeed, rotSpeed);
 	}
 }
