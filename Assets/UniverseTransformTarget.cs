00538004a8268f243f4a284f30836da108e8a217
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-05-16 18:20:42 +0200

    more assigning!

--- Assets/UniverseTransformTarget.cs
+++ Assets/UniverseTransformTarget.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class UniverseTransformTarget : MonoBehaviour {
+public class UniverseTransformTarget : NetworkBehaviour {
 
 	// Use this for initialization
-	void Start () {
-        UniverseTransformer.Instance.transformTarget = this.transform;
+    void OnStartClient()
+    {
+        AssignTargetToUniverse();
 	}
+
+    void OnStartServer()
+    {
+        AssignTargetToUniverse();
+    }
+
+    private void AssignTargetToUniverse()
+    {
+        print("Start");
+        UniverseTransformer.Instance.transformTarget = this.transform;
+    }
 }
