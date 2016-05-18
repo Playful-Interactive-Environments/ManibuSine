da3c2e02a9f7ea981a5e1055de7947b462b98ae6
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-05-18 15:03:43 +0200

    fixfix

--- Assets/SteeringTrigger.cs
+++ Assets/SteeringTrigger.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class SteeringTrigger : MonoBehaviour
+public class SteeringTrigger : NetworkBehaviour
 {
 
     public GameObject assignedPlayer;
