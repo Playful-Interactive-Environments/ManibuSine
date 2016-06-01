5cf89067f5d9b37570066778df1ece06991ba55d
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-06-01 14:38:28 +0200

    changing ship movement to move ship, first test

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+    private static ShipMovement instance;
+    public static ShipMovement Instance { get { return instance; } }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void MoveForward(float speed)
+    {
+        this.transform.Translate(this.transform.forward * speed);
+    }
+
+    void RotateRight(float rot)
+    {
+        this.transform.Rotate(transform.up, rot);
+    }
+}
