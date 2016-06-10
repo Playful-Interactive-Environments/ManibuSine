f5585328a4615f3a1ba952d50a47d769055d9209
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-06-10 16:08:01 +0200

    started game logic (waypoints)

---
+++ Assets/MaSine/Scripts/LevelLogic/WaypointLevel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaypointLevel : MonoBehaviour {
+    [SerializeField]
+    public Waypoint[] waypoints;
+	// Use this for initialization
+	void Start () {
+        waypoints = GetComponentsInChildren<Waypoint>();
+
+        Waypoint.ShipEnteredEvent += ShipEnteredWaypoint;
+        Waypoint.ShipLeftEvent += ShipLeftWaypoint;
+	}
+
+    void ShipEnteredWaypoint(IWaypoint waypoint)
+    {
+        print("WP entered " + waypoint.GetID());
+    }
+
+    private void ShipLeftWaypoint(IWaypoint waypoint)
+    {
+        print("WP exit " + waypoint.GetID());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
