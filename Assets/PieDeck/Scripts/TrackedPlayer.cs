5eb5278e67b9dda6a003a0e87b1281857697ff52
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-06-15 14:20:33 +0200

    changed tracked player set position ( * 0.001f)

--- Assets/PieDeck/Scripts/TrackedPlayer.cs
+++ Assets/PieDeck/Scripts/TrackedPlayer.cs
@@ -7,7 +7,7 @@ public class TrackedPlayer : ATrackingEntity
 	public float Height;
 	public override void SetPosition(Vector2 coords)
 	{
-		this.transform.position = new Vector3(coords.x, Height, coords.y);
+		this.transform.position = new Vector3(coords.x * 0.001f, Height, coords.y * 0.001f);
 	}
 
 	
