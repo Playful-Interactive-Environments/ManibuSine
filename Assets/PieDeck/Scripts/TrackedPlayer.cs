ab8a406c780d5951f34587a7689d4cb7423ecbed
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-06-21 17:26:09 +0200

    tweaking sizes with better knowledge

--- Assets/PieDeck/Scripts/TrackedPlayer.cs
+++ Assets/PieDeck/Scripts/TrackedPlayer.cs
@@ -7,7 +7,7 @@ public class TrackedPlayer : ATrackingEntity
 	public float Height;
 	public override void SetPosition(Vector2 coords)
 	{
-		this.transform.position = new Vector3(coords.x * 0.002f, Height, coords.y * 0.002f);
+		this.transform.position = new Vector3(coords.x * 0.01f, Height, coords.y * 0.01f);
 	}
 
 	
