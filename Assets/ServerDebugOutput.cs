abf93233c160a62b3740a4887bb8fca02da609a2
Daniel Rammer <dnl.rammer@gmail.com>
2016-06-21 18:09:24 +0200

    set tracking resolution in unity added debug text ui on server

---
+++ Assets/ServerDebugOutput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ServerDebugOutput : MonoBehaviour {
+    private static ServerDebugOutput instance;
+    public static ServerDebugOutput Instance
+    {
+        get { return instance; }
+    }
+
+    Text uiText;
+
+	// Use this for initialization
+	void Awake () {
+        instance = this;
+	}
+    void Start() {
+        uiText = GetComponentInChildren<Text>();
+    }
+	
+    public void SetText(string text) {
+        if (uiText == null)
+            return;
+
+        uiText.text = text;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
