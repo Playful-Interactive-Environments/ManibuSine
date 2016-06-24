459c0864532ab6bf5ffe379019584d82e40fb618
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-06-24 14:18:22 +0200

    newer better compass UI

--- Assets/MaSine/Scripts/UI/UI_HeasUpCompas.cs
+++ Assets/MaSine/Scripts/UI/UI_HeasUpCompas.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class UI_HeasUpCompas : MonoBehaviour {
 
+    private float height = 1.7f;
+
     private Transform playerTransform;
-    private Transform waypointTransform = null;
     private ShipManager ship;
     private MeshRenderer mRenderer;
+
+    Hashtable htArrowBounce = new Hashtable();
 
     public void SetPlayer(Transform player)
     {
@@ -19,20 +22,32 @@ public class UI_HeasUpCompas : MonoBehaviour {
         mRenderer.enabled = false;
         ship = FindObjectOfType<ShipManager>();
         WaypointLevel.NextWaypoint += GetNextWaypoint;
+
+	    htArrowBounce.Add("y", 5.2f);
+	    htArrowBounce.Add("time", 2.7f);
+	    htArrowBounce.Add("delay", 0.1f);
+	    htArrowBounce.Add("looptype",iTween.LoopType.pingPong);
+        htArrowBounce.Add("easetype", iTween.EaseType.easeOutElastic);
+
+        iTween.MoveBy(gameObject, htArrowBounce);
     }
 
     private void GetNextWaypoint(IEventTrigger waypoint)
     {
         if (waypoint == null)
         {
-            mRenderer.enabled = false;
-            waypointTransform = null;
+            if (mRenderer != null)
+                mRenderer.enabled = false;
         }
         else
         {
-            mRenderer.enabled = true;
-            waypointTransform = waypoint.GetTransform();
+            if (mRenderer != null)
+                mRenderer.enabled = true;
+
             transform.parent = waypoint.GetTransform();
+            transform.localPosition = Vector3.up * height;
+
+
         }
     }
 	
@@ -40,8 +55,6 @@ public class UI_HeasUpCompas : MonoBehaviour {
 	void Update () {
         if (ship == null)
             return;
-
-
         transform.LookAt(ship.transform);
         //if (playerTransform == null || waypointTransform == null)
         //{
@@ -50,10 +63,6 @@ public class UI_HeasUpCompas : MonoBehaviour {
 
         //    return;
         //}
-
-
-        //transform.position = playerTransform.transform.position;
-
 	}
 
     void Dispose()
