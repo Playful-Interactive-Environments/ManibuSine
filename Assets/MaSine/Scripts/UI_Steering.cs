7ce04600a765ecfd9ab65469e8620a221f471a0a
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-06-24 16:31:13 +0200

    unassigning player from steering delayed + UI countdown. change color of steering station when assigned.

--- Assets/MaSine/Scripts/UI_Steering.cs
+++ Assets/MaSine/Scripts/UI_Steering.cs
@@ -10,6 +10,9 @@ public class UI_Steering : MonoBehaviour {
     private RectTransform rectSteeringCircle;
     public RectTransform rectArrow;
     public RectTransform speedBar;
+    public Text stepOutCountdownText;
+    private float stepOutDuration;
+    private bool doCountDown;
 
     private Vector2 originalScale;
     private Image[] allGraphics;
@@ -28,50 +31,79 @@ public class UI_Steering : MonoBehaviour {
             CancelInvoke("GetSteeringManager");
             steeringManager.EnteredSteering += EnteredSteering;
             steeringManager.ExitedSteering += ExitedSteering;
+            steeringManager.StepedOutSteering += StepedOutSteering;
         }
     }
-
 
     Vector3 oldPos;
     void Update() {
         if (steeringManager == null || steeringManager.navigator == null)
             return;
 
+        CountDown();
+
         if (Mathf.Abs(steeringManager.angleInput) > 90) {
-            ShowGraphics(false);
             return;
         }
 
         ShowGraphics(true);
 
+        AnimateArrow();
+    }
+
+    private void CountDown()
+    {
+        if (!doCountDown)
+            return;
+
+        stepOutDuration -= Time.deltaTime;
+        stepOutCountdownText.text = stepOutDuration.ToString("#.");
+    }
+
+    private void AnimateArrow()
+    {
         float ls = lerpSpeed * Time.deltaTime;
-
-        // speed indicator
-        //if (speedBar != null) {
-        //    Transform tt = UniverseTransformer.Instance.GetTargetTransform();
-        //    if (tt != null) {
-        //        float speed = Vector3.Distance(oldPos, tt.position) * 100;
-        //        speedBar.localScale = Vector3.Lerp(speedBar.localScale, new Vector3(1, speed, 1), ls);
-        //        print("SPEED " + speed);
-        //        oldPos = tt.position;
-        //    }
-
-        //}
-
-
         float clampedSpeed = Mathf.Clamp01(steeringManager.uiArrowLength);
 
-        rectArrow.localRotation = Quaternion.Lerp(rectArrow.localRotation, Quaternion.Euler(0, 0, -steeringManager.angleInput), ls);
+        rectArrow.localRotation = Quaternion.Lerp(rectArrow.localRotation, Quaternion.Euler(0, 0, steeringManager.angleInput), ls);
         rectArrow.localScale = Vector3.Lerp(rectArrow.localScale, new Vector3(1, clampedSpeed, 1), ls);
+    }
+
+    private void StartCountdown()
+    {
+        if (stepOutCountdownText == null)
+            return;
+        stepOutCountdownText.enabled = true;
+        doCountDown = true;
+    }
+
+    private void StopCountdown()
+    {
+        if (stepOutCountdownText == null)
+            return;
+        stepOutCountdownText.enabled = false;
+        doCountDown = false;
     }
 
     private void EnteredSteering(SteeringStation steeringStation) {
         steeringManager = steeringStation;
+        StopCountdown();
         ShowGraphics(true);
+    }
+
+    private void StepedOutSteering(SteeringStation steeringStation)
+    {
+        if (stepOutCountdownText != null)
+        {
+            StartCountdown();
+            stepOutDuration = steeringStation.playerDropOutDelay;
+            rectArrow.localScale = new Vector3(1, 0, 1);
+        }
     }
 
     private void ExitedSteering(SteeringStation steeringStation) {
         steeringManager = null;
+        StopCountdown();
         ShowGraphics(false);
     }
 
@@ -85,6 +117,14 @@ public class UI_Steering : MonoBehaviour {
         originalScale = rectSteeringCircle.localScale;
 
         allGraphics = GetComponentsInChildren<Image>();
+        StopCountdown();
         ShowGraphics(false);
+    }
+
+    void Dispose()
+    {
+        steeringManager.EnteredSteering -= EnteredSteering;
+        steeringManager.ExitedSteering -= ExitedSteering;
+        steeringManager.StepedOutSteering -= StepedOutSteering;
     }
 }
