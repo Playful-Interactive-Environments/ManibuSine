720605709f96527313af83909842ef4b4b38a80a
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-06-25 10:33:31 +0200

    implemented do/cross visualization

--- Assets/MaSine/Scripts/UI_TargetingDot.cs
+++ Assets/MaSine/Scripts/UI_TargetingDot.cs
@@ -1,22 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UI_TargetingDot : MonoBehaviour {
 
     private float initialDistance;
     private float initialScale;
     private RectTransform rectTransform;
+    private Image image;
+
+    private Color originalColor;
+    public Color denyColor;
+    public Sprite dot, cross;
 
 	// Use this for initialization
 	void Start () {
         rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
         initialDistance = rectTransform.position.z;
         initialScale = rectTransform.localScale.x;
+
+        originalColor = image.color;
+
+        CannonPivot.OutOfRange += OnOutOfRange;
+        CannonPivot.InRange += OnInRange;
 	}
+
+    private void OnInRange()
+    {
+        image.color = originalColor;
+        image.sprite = dot;
+    }
+
+    private void OnOutOfRange()
+    {
+        image.color = denyColor;
+        image.sprite = cross;
+    }
 
     public void ResetDot()
     {
         rectTransform.localPosition = new Vector3(0, 0, initialDistance);
         rectTransform.localScale = Vector3.one * initialScale;
+    }
+
+    void Dispose()
+    {
+        CannonPivot.OutOfRange -= OnOutOfRange;
+        CannonPivot.InRange -= OnInRange;
     }
 }
