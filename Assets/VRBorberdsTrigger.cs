0bbf80cede418bf816df8854a84d0714f63f7a68
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-07-13 17:00:51 +0200

    added mesh to save people from certain death

---
+++ Assets/VRBorberdsTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRBorberdsTrigger : MonoBehaviour {
+
+    public GameObject vrBorderParent;
+    private MeshRenderer[] meshes;
+	// Use this for initialization
+	void Start () {
+        meshes = vrBorderParent.GetComponentsInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        foreach(MeshRenderer mesh in meshes)
+        {
+            mesh.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        foreach (MeshRenderer mesh in meshes)
+        {
+            mesh.enabled = false;
+        }
+    }
+}
