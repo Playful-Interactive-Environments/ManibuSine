a19faeb0248bc9c83ecb0e6821f028d9e5ad3847
unknown <Daniel@F073PC.hagenberg.fhooe.at>
2016-07-19 13:49:46 +0200

    fixed collision border visualization

--- Assets/VR_CylinderBorder.cs
+++ Assets/VR_CylinderBorder.cs
@@ -5,25 +5,32 @@ public class VR_CylinderBorder : MonoBehaviour {
 
     private MeshRenderer mesh;
     private NetworkPlayer thisPlayer;
-
+    public void AssignPlayer(NetworkPlayer player)
+    {
+        thisPlayer = player;
+    }
 
     void Start() {
         mesh = GetComponent<MeshRenderer>();
         mesh.enabled = false;
     }
 
-    private bool IsOwnGraphic(Collider other) {
-        return (other.tag != "Player" || !thisPlayer.isLocalPlayer);
+    private bool ShowGraphic(Collider other) {
+        if (thisPlayer == null)
+            return false; // happens on ohter player
+
+        return (other.tag == "Player" && other.gameObject != thisPlayer.gameObject);
     }
 
     void OnTriggerEnter(Collider other) {
-        if (!IsOwnGraphic(other))
+        if (!ShowGraphic(other))
             return;
+
         mesh.enabled = true;
     }
 
     void OnTriggerExit(Collider other) {
-        if (!IsOwnGraphic(other))
+        if (!ShowGraphic(other))
             return;
         mesh.enabled = false;
     }
