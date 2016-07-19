94fd38b1a2763b6cf54fb91c2854d3427f13c0e1
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-07-19 16:09:01 +0200

    change click check for tracked player

--- Assets/PieDeck/Scripts/TrackedPlayerNetworkBehaviour.cs
+++ Assets/PieDeck/Scripts/TrackedPlayerNetworkBehaviour.cs
@@ -11,31 +11,49 @@ public class TrackedPlayerNetworkBehaviour : NetworkBehaviour {
 
 	public GameObject ControlledPlayer;
 
-	void Start () {
+    public float distanceLimit = 10000f;
+    public LayerMask whatToCollideWith;
+
+    void Start () {
 
 	}
-	
 
-	void Update () {
-		if (ServerManager.Instance.isServer)
-		{
-			NetworkServer.Spawn(gameObject);
-			this.transform.FindChild("Server").gameObject.SetActive(true);
-			this.transform.FindChild("Client").gameObject.SetActive(false);
-		}
-		if (ServerManager.Instance.isClient)
-		{
-			this.transform.FindChild("Server").gameObject.SetActive(false);
-			if (HasPlayerOne || HasPlayerTwo)
-			{
-				this.transform.FindChild("Client").gameObject.SetActive(false);
-			}
-			if (!HasPlayerOne && !HasPlayerTwo)
-			{
-				this.transform.FindChild("Client").gameObject.SetActive(true);
-			}
-		}
-	}
+
+    void Update()
+    {
+        if (ServerManager.Instance.isServer)
+        {
+            NetworkServer.Spawn(gameObject);
+            this.transform.FindChild("Server").gameObject.SetActive(true);
+            this.transform.FindChild("Client").gameObject.SetActive(false);
+        }
+        if (ServerManager.Instance.isClient)
+        {
+            this.transform.FindChild("Server").gameObject.SetActive(false);
+            if (HasPlayerOne || HasPlayerTwo)
+            {
+                this.transform.FindChild("Client").gameObject.SetActive(false);
+            }
+            if (!HasPlayerOne && !HasPlayerTwo)
+            {
+                this.transform.FindChild("Client").gameObject.SetActive(true);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, distanceLimit, whatToCollideWith))
+            {
+                if (hit.transform.gameObject != this.gameObject)
+                    return;
+
+                Admin.Instance.CurrentTrackedPlayer = hit.transform.gameObject;
+                StartCoroutine("ChangeColor");
+            }
+        }
+    }
 	void OnDestroy()
 	{
 		if (ServerManager.Instance.isServer)
@@ -52,8 +70,8 @@ public class TrackedPlayerNetworkBehaviour : NetworkBehaviour {
 	}
 	void OnMouseDown()
 	{
-        Admin.Instance.CurrentTrackedPlayer = gameObject;
-			StartCoroutine("ChangeColor");
+            //Admin.Instance.CurrentTrackedPlayer = gameObject;
+			//StartCoroutine("ChangeColor");
 	}
 
 	IEnumerator ChangeColor()
---
+++ Assets/ScreenManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("displays connected: " + Display.displays.Length);
+        // Display.displays[0] is the primary, default display and is always ON.
+        // Check if additional displays are available and activate each.
+        if (Display.displays.Length > 1)
+        {
+            Display.displays[1].Activate();
+            Display.displays[1].SetParams(Display.displays[1].systemWidth, Display.displays[1].systemHeight, Display.displays[0].systemWidth, 0);
+        }
+            
+        if (Display.displays.Length > 2)
+            Display.displays[2].Activate();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
