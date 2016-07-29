f3f06ea40acbf71679f27e776081f281f322607d
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-07-29 11:34:28 +0200

    adjusted tracked player offset to fit piespace

--- Assets/PieDeck/Scripts/TrackedPlayer.cs
+++ Assets/PieDeck/Scripts/TrackedPlayer.cs
@@ -4,9 +4,12 @@ using UnityEngine.Networking;
 
 public class TrackedPlayer : ATrackingEntity
 {
+    //add offset to adjust tracking area without changing the whole setup
+    private float offsetX = 5.275f;
+    private float offsetY = 0;
 	public float Height;
 	public override void SetPosition(Vector2 coords)
 	{
-		this.transform.position = new Vector3(coords.x * 0.01f, Height, coords.y * 0.01f);
+		this.transform.position = new Vector3(coords.x * 0.01f + offsetX, Height, coords.y * 0.01f + offsetY);
 	}
 }
