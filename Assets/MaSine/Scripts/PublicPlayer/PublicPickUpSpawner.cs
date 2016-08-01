c7b4e1e525a24603cc3e820c187eab49bffff7f1
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-08-01 16:35:58 +0200

    pickupspawner stops spawning

--- Assets/MaSine/Scripts/PublicPlayer/PublicPickUpSpawner.cs
+++ Assets/MaSine/Scripts/PublicPlayer/PublicPickUpSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PublicPickUpSpawner : MonoBehaviour {
     private static int pickUpCounter = 0;
@@ -8,12 +9,17 @@ public class PublicPickUpSpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        ShipManager.GameOver += OnGameOver;
         InvokeRepeating("SpawnPU", 0.1f, 20);
 	}
-	
-	// Update is called once per frame
-	void SpawnPU () {
+
+    private void OnGameOver(int damage)
+    {
+        CancelInvoke("SpawnPU");
+    }
+
+    // Update is called once per frame
+    void SpawnPU () {
         ServerManager.Instance.SpawnPickUp(new Vector3(4.68f, 1.96f, 8.4f));
 	}
 }
