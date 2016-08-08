f85c0e74c9af6030063395f31402e646a63ce60f
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-08-08 15:04:47 +0200

    added visual feedback to world collision

---
+++ Assets/MaSine/Scripts/WorldCollider.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldCollider : MonoBehaviour {
+
+    public GameObject particle;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionStay(Collision collision)
+    {
+        //print("enter" + collision.contacts[0].thisCollider.GetComponent<MeshRenderer>());
+        MeshRenderer mr = collision.contacts[0].thisCollider.GetComponent<MeshRenderer>();
+        if (mr != null)
+        {
+            //mr.enabled = true;
+            particle.transform.position = collision.contacts[0].point;
+            particle.GetComponent<ParticleSystem>().Play();
+        }
+            
+
+            
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        //print("exit");
+        MeshRenderer[] mr = GetComponentsInChildren<MeshRenderer>();
+        foreach(MeshRenderer meshRenderer in mr)
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+}
