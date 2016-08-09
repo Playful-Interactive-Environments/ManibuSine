c8cd48b77b96e3b81a58e8ef6f4c874feccd9f38
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-08-09 16:08:13 +0200

    changed delegates to always unregister

--- Assets/MaSine/Scripts/UI/UI_Steering.cs
+++ Assets/MaSine/Scripts/UI/UI_Steering.cs
@@ -104,8 +104,8 @@ public class UI_Steering : MonoBehaviour {
 
     void OnDestroy()
     {
-        if (steeringManager == null)
-            return;
+        //if (steeringManager == null)
+        //    return;
         SteeringStation.EnteredSteering -= OnEnteredSteering;
         SteeringStation.ExitedSteering -= OnExitedSteering;
     }
