e22c572dec233be6a9c793a5a57682e474806d3f
F073PC\Daniel <dnl.rammer@gmail.com>
2016-08-10 17:30:27 +0200

    set delays - deleted outputs

--- Assets/ClientRestarter.cs
+++ Assets/ClientRestarter.cs
@@ -10,7 +10,6 @@ public class ClientRestarter : MonoBehaviour {
 
     IEnumerator RestartDelayed() {
         yield return new WaitForSeconds(1.5f);
-        print("do it boy");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
