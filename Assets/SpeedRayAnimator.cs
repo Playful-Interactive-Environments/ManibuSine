284417e65e1955540572270db78fc1096fd86749
unknown <dnl.rammer@gmail.com>
2016-08-30 18:21:21 +0200

    speed rays stage 1 :)

---
+++ Assets/SpeedRayAnimator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedRayAnimator : MonoBehaviour {
+    private float animationCurrent;
+    private float animationSpeed = 1.0f;
+
+
+    // TODO: do this for all materials (2 children)
+    private Material mat;
+
+    void Start () {
+        mat = GetComponentInChildren<MeshRenderer>().material;
+    }
+	
+	void Update () {
+        if (animationCurrent < 1) {
+            animationCurrent += Time.deltaTime * animationSpeed;
+        }
+        else {
+            animationCurrent = 0;
+        }
+
+        mat.mainTextureOffset = Vector2.up * animationCurrent;
+    }
+}
