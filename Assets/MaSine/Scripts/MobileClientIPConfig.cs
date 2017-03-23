b0a95fd90852e3f5df93419525542bc6e36d2784
Daniel Rammer <dnl.rammer.@gmail.com>
2017-03-23 13:32:16 +0100

    added build to repo; changed ip to 192.168.1.2

---
+++ Assets/MaSine/Scripts/MobileClientIPConfig.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.IO;
+
+public class MobileClientIPConfig : MonoBehaviour
+{
+    private string clientIP;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    private bool ConfigureRenderClient()
+    {
+        string path = Path.Combine(Application.persistentDataPath, "RenderClientConfig.xml");
+        XmlDocument doc = new XmlDocument();
+        try
+        {
+            doc.Load(path);
+        }
+        catch (FileNotFoundException e)
+        {
+            return false;
+        }
+
+        foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+        {
+            // read ip adress
+            if (node.Name == "ip")
+            {
+                clientIP = node.InnerText;
+            }
+        }
+        return true;
+    }
+}
